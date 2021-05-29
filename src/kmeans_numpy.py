1cc3d68313de0f39c4d435c5080d0f85bea97131
Marcin Kuropatwiński <marcin@talking2rabbit.com>
2021-05-29 13:45:11 +0200

    finished kmeans with docu

--- src/kmeans_numpy.py
+++ src/kmeans_numpy.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+kmeans_numpy.py
+=============================================================================
+Implements the kmeans algorithm for finding an optimal codebook. Uses kmeans++\
+as initialization algorithm.
+"""
 
 import random
+import site
 
+site.addsitedir("../kmeanspp/src")
+import kmeanspp_numpy
 import numpy as np
-import numba
 
 class KMeans:
 
-    def __init__(self, train_set, initial_codebook):
+    """ Class for training of the codebook based on a training set and an initial codebook. Uses the\
+    KMeans algorithm.
+
+    :param train_set: training set with training vectors as columns, the computations precision is inherided from the\
+    numeric type of the training vectors
+    :type train_set: np.ndarray
+    :param initial_codebook: initial codebook, prepared using e.g. KMeans++ algorith, centroids as columns
+    :type initial_codebook: np.ndarray
+    """
+    def __init__(self, train_set : np.ndarray, initial_codebook : np.ndarray):
+        """Constructor method"""
 
         self.train_set = train_set
 
         # The number of training points is assumed to be larger than dimension \
         # of the training points
 
-        if train_set.shape[0] > train_set.shape[1]:
-
-            self._train_set = np.transpose(train_set) # force training points to be columns
+        # force training points to be columns
+        self._train_set = np.transpose(train_set) if train_set.shape[0] > train_set.shape[1] else train_set
 
         # check dimension of the initial_codebook
-        if not self._train_set.shape[0] in set([self._initial_codebook.shape[0], self._initial_codebook.shape[1]):
+        if not self._train_set.shape[0] in set([initial_codebook.shape[0], initial_codebook.shape[1]]):
             raise ValueError("Dimension of codevectors in the inial codebook is unproper...")
 
         # transpose the inital codebook if needed, force the numeric type of initial codebook to be\
@@ -39,23 +56,12 @@ class KMeans:
 
 
     def _permute(self, sortin, permutation):
-        '''
-        Funkcja przeprowadzająca operację permutacji\n
-        :param sortin: lista zmiennych do permutacji\n
-        :param permutation: permutacja\n
-        :return: lista po permutacji\n
-        :rtype: list
-        '''
+
         assert len(sortin) == len(permutation)
         return [sortin[i] for i in permutation]
 
     def _my_sort(self, inputlist):
-        '''
-        Funkcja sortująca sygnatury interwałów\n
-        :param inputlist: lista\n
-        :return: 3 listy: posortowanych sygnatur, kolejność w jakiej należy ustawić sygnatury aby uzyskać kolejność po posortowaniu oraz przywrócić kolejność sprzed sortowania
-        :rtype: list        
-        '''
+
         inputlist = zip(inputlist, range(len(inputlist)))
         aux = sorted(inputlist, key=lambda x: x[0])
         sorted2in = [aux[i][1] for i in range(len(aux))]
@@ -65,13 +71,13 @@ class KMeans:
         sort = [aux[i][0] for i in range(len(aux))]
         return sort, in2sorted, sorted2in
 
-    def _diffs(self):
+    def _get_diffs(self):
         # below statement results in KxMxd tensor, K - number of codevectors, M- number of training points\
         # d - dimension of the train points
         self._diffs = self._codebook[np.newaxis,...].transpose((2,0,1)) - \
                                                self.train_set[np.newaxis,...].transpose((0,2,1))
 
-    def _distances_squared(self):
+    def _get_distances_squared(self):
 
         self._distances_squared = np.sum(self._diffs * self._diffs, axis = 2)
 
@@ -85,7 +91,6 @@ class KMeans:
 
         self._distortion = np.sqrt(np.sum(np.amin(self._distances_squared,axis = 0))/(self._M*self._d))
 
-    @numba.njit
     def _update_codebook(self):
 
         self._number_of_train_points_per_voronoi_region = np.zeros((self._codebook.shape[1],))
@@ -96,13 +101,14 @@ class KMeans:
             
             self._number_of_train_points_per_voronoi_region[k] = np.sum(self._idxs == k)
 
-            if not self._number_of_train_points_per_voronoi_region[k] == 0
+            if not self._number_of_train_points_per_voronoi_region[k] == 0:
                                                 
                 self._codebook[:,k] = 1/self._number_of_train_points_per_voronoi_region[k] * np.sum(self._train_set[:,self._idxs == k], axis = 1)
 
             else:
 
                 self._empty_voronoi_regions.append(k)
+
                                                 
     def _handle_empty_voronoi_regions(self):
         
@@ -112,7 +118,7 @@ class KMeans:
 
         AUX_MULTIPLICATOR = 2
 
-        while np.sum(self._number_of_train_points_per_voronoi_region[-i:]) < AUX_MULTIPLICATOR * len(self._empty_voronoi_regions):
+        while np.sum(aux1[-i:]) < AUX_MULTIPLICATOR * len(self._empty_voronoi_regions):
 
             i += 1
 
@@ -124,7 +130,7 @@ class KMeans:
 
         # get candidate train_points to replace the centroids of emtpy Voronoi regions
 
-        samples = random.sample(np.arange(candidates_for_replacing_centroids_of_empty_cells.shape[1]), len(self._empty_voronoi_regions))
+        samples = random.sample(list(range(candidates_for_replacing_centroids_of_empty_cells.shape[1])), len(self._empty_voronoi_regions))
 
         # replace the centroids
 
@@ -133,30 +139,53 @@ class KMeans:
             self._codebook[:,self._empty_voronoi_region[i]] = self._train_set[:,sample]
             
 
-    def train(self):
+    def train(self, convergence_criterion : float = 1e-4) -> np.ndarray:
+        """The main method of the class. The method trains actually the codebook.
 
-        for i in range(10):
+        :param convergence_criterion: convergence criterion (D_prev - D)/D < convergence_criterion, defaults to 1e-4
+        :type convergence_criterion: float
+        :return: the method returs the trained codebook with centroids as columns
+        :rtype: np.ndarray
+        """
 
-            self._diffs()
+        iteration_num = 0
 
-            self._distances_squared()
+        while True:
+
+            self._get_diffs()
+
+            self._get_distances_squared()
 
             self._get_distortion()
 
-            print(f"Distortion per training vector per dimension at iteration {i} is: {self._distortion}")
+            if (self._distortion_prev - self._distortion)/self._distortion < convergence_criterion:
+
+                return self._codebook
+
+            print(f"Distortion per training vector per dimension at iteration {iteration_num} is: {self._distortion}")
+
+            iteration_num += 1
 
             self._assign_train_vectors_to_voronoi_regions()
 
+            
             self._update_codebook()
 
-            self._handle_empty_voronoi_regions()
+            if self._empty_voronoi_regions:
+
+                self._handle_empty_voronoi_regions()
 
             
 if __name__ == "__main__":
 
-    train_set = np.random.randn((10,1000000))
+    train_set = np.random.randn(10,1000000).astype(np.float32)
     
-     
+    initial_codebook = kmeanspp_numpy.kmeanspp(train_set,100)     
+
+    codebook = KMeans(train_set,initial_codebook)
+
+    cb = codebook.train()
+
 
 
 
@@ -171,9 +200,6 @@ if __name__ == "__main__":
         
 
         
-
-
-
 
 
 
